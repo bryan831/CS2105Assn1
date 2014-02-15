2dff8fc0e80b0cd08f957e0fc39e87b332510d29
bryan831 <bryanchubh@gmail.com>
2014-02-15 14:06:48 +0800

    Created Connection class
    Connection class is called by the WebServer class when a new connection
    is received

--- WebServer.java
+++ WebServer.java
@@ -20,7 +20,7 @@ class WebServer {
 		System.out.println("In the WebServer now.");
 		System.out.println("Creating ServerSocket...");
 		try {
-			serverSocket = new ServerSocket(listenPort);
+			serverSocket = new ServerSocket(listenPort);	//WebServer sets up a new ServerSocket at the port #
 		}
 		catch (IOException e)
 		{
@@ -34,9 +34,10 @@ class WebServer {
 				Socket socket1;
 				socket1 = serverSocket.accept();
 				System.out.println("Connection accepted.");
+				//once the ServerSocket receives a connection, it is accepted and the Connection class is called
 				new Connection(socket1);
 				
-				//continue;
+				continue;
 			}
 			catch (IOException ioE)
 			{
@@ -73,31 +74,69 @@ class WebServer {
 
 }	//end of WebServer class
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Connection 
 {
 	String filename;
 	File file;
 	static String WEB_ROOT = "";
+
+	Socket socket;
+	InputStream is;
+	BufferedReader br;
+	OutputStream os;
+	DataOutputStream dos;
+
+	String line;
+	String tokens[];
+
 	public Connection (Socket socketIn) 
 	{	//constructor for the Connection class
-		Socket socket;
-		//connection has been accepted, set up the input and output streams 
+
 		socket = socketIn;
+		//connection has been accepted, set up the input and output streams 
 		try 
 		{
-			InputStream is = socket.getInputStream();
-			BufferedReader br = new BufferedReader(new InputStreamReader(is));
+			is = socket.getInputStream();
+			br = new BufferedReader(new InputStreamReader(is));
 
-			OutputStream os = socket.getOutputStream();
-			DataOutputStream dos = new DataOutputStream(os);
+			os = socket.getOutputStream();
+			dos = new DataOutputStream(os);
 			System.out.println("Your IS, BR, OS, and DOS have been set up.");
 
 			//start reading lines from the socket
 			while (true)
 			{
-				String line = br.readLine();
-
-				String tokens[] = line.split(" ");
+				line = br.readLine();
+				tokens = line.split(" ");
 
 				//if the first token is NOT GET AND NOT POST
 				if (!tokens[0].equals("GET") && !tokens[0].equals("POST"))
@@ -107,7 +146,8 @@ class Connection
 					dos.writeBytes("Content-length: " + errorMessage.length() + "\r\n\r\n");
 					dos.writeBytes(errorMessage);
 					socket.close();
-					continue;
+					//continue;
+					return;
 				}
 				if (line == null)
 				{
@@ -118,76 +158,107 @@ class Connection
 				if (tokens[0].equals("GET") && (tokens.length>1))	//if first token is GET, and length>1, assume token[1] is filename 
 				{
 					System.out.println("GET " + tokens[1]);
-					filename = WEB_ROOT + tokens[1];
-					file = new File(filename);
-					if (!file.exists()) 
-					{
-						String errorMessage = "I cannot find " + tokens[1] + " on this server.\r\n";
-						dos.writeBytes("HTTP/1.1 404 Not Found\r\n");
-						dos.writeBytes("Content-length: " + errorMessage.length() + "\r\n\r\n");
-						dos.writeBytes(errorMessage);
-						socket.close();
-						continue;
-					}
-					if (!file.canRead()) 
-					{
-						String errorMessage = "You have no permission to access " + tokens[1] + " on this server.\r\n";
-						dos.writeBytes("HTTP/1.1 403 Forbidden\r\n");
-						dos.writeBytes("Content-length: " + errorMessage.length() + "\r\n\r\n");
-						dos.writeBytes(errorMessage);
-						socket.close();
-						continue;
-					}
-					//at this point, everything is OK
-					dos.writeBytes("HTTP/1.1 200 OK\r\n");
-					//send back content length
-					dos.writeBytes("Content-length: " + file.length() + "\r\n");
-					//send back content type:
-					if (filename.endsWith(".html")) 
-					{
-						dos.writeBytes("Content-type: text/html\r\n");
-					}
-					if (filename.endsWith(".jpg")) 
-					{
-						dos.writeBytes("Content-type: image/jpeg\r\n");
-					}	
-					dos.writeBytes("\r\n");
+					handleGET();
 
-					// the body of the file.
-				
-					// Read the content 1KB at a time.
-					byte[] buffer = new byte[1024];
-					FileInputStream fis = new FileInputStream(file);
-					int size = fis.read(buffer);						//reads from FileInputStream fis and stores into buffer
-					while (size > 0) 
-					{
-						dos.write(buffer, 0, size);						//writes to the DataOutputStream with args (byte[], starting offset, length)
-						size = fis.read(buffer);
-					}
-					dos.flush();										//flushes the DataOutputStream, forces buffered bytes to be written out
-
-					// Finally, close the socket and get ready for
-					// another connection.
-
-					socket.close();
-
+					return;	
 				}
-								
-
 				//System.out.println("Server is going to writeBytes");
 				else if (tokens[0].equals("POST"))
 				{
 					System.out.println("In POST");
+					
+
 					socket.close();
 
 
 				}
 				return;
-			}
+			}		//end of while(true)
 		}	//end of try
 		catch (IOException e)
 		{
 			System.err.println("Unable to read/write: " + e.getMessage());
 		}
 	}	//end of constructor
+
+
+	void handleGET () 
+	{	//the handleGet function handles the HTTP GET requests in Connection class
+		try 
+		{
+			filename = WEB_ROOT + tokens[1];
+			file = new File(filename);
+
+			//first check if the file requested is the dynamic Perl script
+			if (filename.endsWith(".pl"))
+			{
+				//need to execute the Perl script instead of sending the content back
+
+			}
+
+
+			if (!file.exists()) 
+			{
+				String errorMessage = "I cannot find " + tokens[1] + " on this server.\r\n";
+				dos.writeBytes("HTTP/1.1 404 Not Found\r\n");
+				dos.writeBytes("Content-length: " + errorMessage.length() + "\r\n\r\n");
+				dos.writeBytes(errorMessage);
+				socket.close();
+				
+				return;
+			}
+			if (!file.canRead()) 
+			{
+				String errorMessage = "You have no permission to access " + tokens[1] + " on this server.\r\n";
+				dos.writeBytes("HTTP/1.1 403 Forbidden\r\n");
+				dos.writeBytes("Content-length: " + errorMessage.length() + "\r\n\r\n");
+				dos.writeBytes(errorMessage);
+				socket.close();
+			
+				return;
+			}
+			//at this point, everything is OK
+			dos.writeBytes("HTTP/1.1 200 OK\r\n");
+			//send back content length
+			dos.writeBytes("Content-length: " + file.length() + "\r\n");
+			//send back content type:
+			if (filename.endsWith(".html")) 
+			{
+				dos.writeBytes("Content-type: text/html\r\n");
+			}
+			if (filename.endsWith(".jpg")) 
+			{
+				dos.writeBytes("Content-type: image/jpeg\r\n");
+			}	
+			if (filename.endsWith(".gif"))
+			{
+				dos.writeBytes("Content-type: image/gif\r\n");
+			}
+			dos.writeBytes("\r\n");
+
+			// the body of the file.
+		
+			// Read the content 1KB at a time.
+			byte[] buffer = new byte[1024];
+			FileInputStream fis = new FileInputStream(file);
+			int size = fis.read(buffer);						//reads from FileInputStream fis and stores into buffer
+			while (size > 0) 
+			{
+				dos.write(buffer, 0, size);						//writes to the DataOutputStream with args (byte[], starting offset, length)
+				size = fis.read(buffer);
+			}
+			dos.flush();										//flushes the DataOutputStream, forces buffered bytes to be written out
+
+			// Finally, close the socket and get ready for
+			// another connection.
+
+			socket.close();
+
+			return;
+		}	//end of try
+		catch (IOException e)
+		{
+			System.err.println("Unable to read/write: " + e.getMessage());
+		}
+	}		//end of handleGet function
 }	//end of Class Connection
